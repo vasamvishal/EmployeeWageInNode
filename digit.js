be249c4f77ff779591f9092a7ccc8d05f8140d6f
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-12-26 15:03:49 +0530

    ADDED:Code for displaying words based on number

---
+++ digit.js
@@ -0,0 +1,46 @@
+var number=process.argv[2];
+if(number==0)
+{
+console.log("ZERO");
+}
+else if(number==1)
+{
+console.log("ONE");
+}
+else if(number==2)
+{
+console.log("TWO");
+}
+else if(number==3)
+{
+console.log("THREE");
+}
+else if(number==4)
+{
+console.log("FOUR");
+}
+else if(number==5)
+{
+console.log("FIVE");
+}
+else if(number==6)
+{
+console.log("SIX");
+}
+else if(number==7)
+{
+console.log("SEVEN");
+}
+else if(number==8)
+{
+console.log("EIGHT");
+}
+else if(number==9)
+{
+console.log("NINE");
+}
+else
+{
+console.log("Wrong number entered");
+}
+
