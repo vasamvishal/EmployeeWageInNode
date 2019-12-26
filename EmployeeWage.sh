9bd7f9f26d62d46ea7c8592593531546276fceb4
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-12-26 10:51:22 +0530

    ADD:Code to calculate empWage for 8 hours

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,7 +1,12 @@
-
+let EMP_RATE_PER_HOURS=20;
+let empHrs=0;
+let empWage=0;
 let isPresent=1;
+
 let empCheck=Math.floor(Math.random()*2);
 if(empCheck ==1 )
-        console.log("Employee is present");
+        empHrs=8;
 else
-        console.log("Employee is not present");
+        empHrs=0;
+empWage=empHrs*EMP_RATE_PER_HOURS;
+console.log(empWage);
