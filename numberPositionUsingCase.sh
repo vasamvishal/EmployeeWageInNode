c3fb009544b248b8f02d0dd2e788b788cef5c063
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-12-26 16:12:40 +0530

    ADD:Code for switch case added

---
+++ numberPositionUsingCase.sh
@@ -0,0 +1,19 @@
+var number=process.argv[2];
+switch(number)
+{
+case "1":
+	console.log("Unit");
+	break;
+case "10":
+	console.log("Ten");
+	break;
+case "100":
+	console.log("hundred");
+	break;
+case "1000":
+	console.log("thoushand");
+	break;
+default:
+	console.log("Wrong number entered");
+}
+
